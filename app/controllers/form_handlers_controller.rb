45693b387133e652b3d7be10e0e728b02dfc1adb
Mikhail Ulyanov <m.ulyanov96@mail.ru>
2017-07-17 10:43:04 +0300

    Fix Message.count

--- app/controllers/form_handlers_controller.rb
+++ app/controllers/form_handlers_controller.rb
@@ -8,12 +8,12 @@ class FormHandlersController < ApplicationController
     @message = Message.new(params.require(:message).permit(:text))
     respond_to do |format|
       if @message.save
-        format.html { redirect_to @message, notice: 'Message was Created.'}
+        format.html { redirect_to @message, notice: 'Message was Created.' }
         format.js
-        format.json {render json: @message, status: :created, location: @message}
+        format.json { render json: @message, status: :created, location: @message }
       else
-        format.html{ render action: "new"}
-        format.json{ render json: @message.errors, status: :unprocessable_entity}
+        format.html { render action: "new" }
+        format.json { render json: @message.errors, status: :unprocessable_entity }
       end
     end
   end
